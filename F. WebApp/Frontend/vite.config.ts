1051f26254f44d50163d5f5f64d021e8ae9a602f
dickyfp6 <147455930+dickyfp6@users.noreply.github.com>
2026-06-09 19:05:11 +0700

    bismillah

--- F. WebApp/Frontend/vite.config.ts
+++ F. WebApp/Frontend/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
+      injectRegister: 'inline',
       includeAssets: ['favicon.svg', 'icons.svg', '192.png', '512.png'],
       manifest: {
         name: 'ChronoBite',
@@ -23,17 +24,17 @@ export default defineConfig({
         scope: '/',
         icons: [
           {
-            src: '192.png',
+            src: '/192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '512.png',
+            src: '/512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: '512.png',
+            src: '/512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
