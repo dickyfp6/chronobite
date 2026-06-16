8be226f6c6c4a593f5893c7599312578fe418e75
silfiameiw <silfiameiw101@gmail.com>
2026-06-16 18:13:19 +0700

    hmm

--- D. Model/Genetic Algorithm/ga_config.py
+++ D. Model/Genetic Algorithm/ga_config.py
@@ -1,7 +1,7 @@
 GA_PARAMS = {
-    "generations": 70,
-    "pop_size": 110,
+    "generations": 40,
+    "pop_size": 70,
     "elite_ratio": 0.10,
     "mutation_rate": 0.25,
 }
-LS_PARAMS = {"iterations": 30}
+LS_PARAMS = {"iterations": 15}
