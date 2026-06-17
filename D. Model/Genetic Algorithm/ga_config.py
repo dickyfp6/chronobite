ae887a43324cafaddb73eeb46f7fe57cc876fe12
dickyfp6 <147455930+dickyfp6@users.noreply.github.com>
2026-06-17 13:41:21 +0700

    Update GA to Trial 59 parameters (Balanced speed & accuracy)

--- D. Model/Genetic Algorithm/ga_config.py
+++ D. Model/Genetic Algorithm/ga_config.py
@@ -8,13 +8,22 @@
 # LS_PARAMS = {"iterations": 30}
 
 # New parameters (Optimized from Kaggle Tuning - Trial 147)
+# GA_PARAMS = {
+#     "generations": 270,
+#     "pop_size": 70,
+#     "elite_ratio": 0.05,
+#     "mutation_rate": 0.55,
+# }
+# LS_PARAMS = {"iterations": 35}
+
+# Balanced parameters (Kaggle Tuning - Trial 59)
 GA_PARAMS = {
-    "generations": 270,
-    "pop_size": 70,
-    "elite_ratio": 0.05,
-    "mutation_rate": 0.55,
+    "generations": 100,
+    "pop_size": 50,
+    "elite_ratio": 0.30,
+    "mutation_rate": 0.10,
 }
 
 LS_PARAMS = {
-    "iterations": 35 
+    "iterations": 30 
 }
